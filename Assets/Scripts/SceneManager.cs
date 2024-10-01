d2b93e84ebee079658eb8ca2befe7e29cdef0a30
GangHyeon <128055955+Lepied@users.noreply.github.com>
2024-10-01 23:23:09 +0900

    feat:씬매니저 코드 변경
    언로드씬 추가.
    추후 씬 관리할때 이 기능 사용
    
    현재 게임 씬에 캔버스가 따로있어서 오류발생.
    해결방법
    릴레이를 통해 서버연결하여 게임씬에서 서버연결을 위한 캔버스를 없애면 해결가능

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -6,12 +6,60 @@ using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    // Start is called before the first frame update
- 
-    public void StartGame()
+    private static SceneManager instance;
+
+    private void Awake()
     {
-        SceneLoadData gameSceneData = new SceneLoadData(sceneName: "Game");
-        gameSceneData.ReplaceScenes = ReplaceOption.All;
-        InstanceFinder.SceneManager.LoadGlobalScenes(gameSceneData);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject); // Destroy duplicate instance
+        }
+    }
+
+    private void Update()
+    {
+        if (!InstanceFinder.IsServer)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            UnloadScene("Game");
+            LoadScene("Main");
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            UnloadScene("Main");
+            LoadScene("Game");        
+        }
+
+    }
+    public void LoadScene(string sceneName)
+    {
+        if (!InstanceFinder.IsServer)
+        {
+            return;
+        }
+
+        SceneLoadData sld = new SceneLoadData(sceneName);
+        //sld.ReplaceScenes = ReplaceOption.All;
+        InstanceFinder.SceneManager.LoadGlobalScenes(sld);
+    }
+
+    public void UnloadScene(string sceneName)
+    {
+        if (!InstanceFinder.IsServer)
+        {
+            return;
+        }
+
+        SceneUnloadData sld = new SceneUnloadData(sceneName);
+        InstanceFinder.SceneManager.UnloadGlobalScenes(sld);
     }
 }
